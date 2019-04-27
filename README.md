2678f192b5e4a6db53af1a450e8cd6f79f4306d8
Peter Kumaschow <pkumaschow@gmail.com>
2019-04-27 22:35:05 +1000

    Updated README

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ https://machinelearningmastery.com/use-pre-trained-vgg-model-classify-objects-ph
 
 Except I'm using VGG19 instead of VGG16. Predictions are more accurate.
 
+Image: 47654254552_6b9a395051_z.jpg
+VGG16: great_grey_owl (64.54%) 
+VGG19: great_grey_owl (95.63%)
+
 Besides the python requirements it will help to have Nvidia GPU and Cuda Toolkit installed.
 
 I'm using a 1060.
--- predict.py
+++ predict.py
@@ -1,12 +1,12 @@
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
-from keras.applications.vgg19 import preprocess_input
-from keras.applications.vgg19 import decode_predictions
-from keras.applications.vgg19 import VGG19
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+from keras.applications.vgg16 import VGG16
 # load the model
-model = VGG19()
+model = VGG16()
 # load an image from file
-image = load_img('images/32763874587_d4e59cbfab_z.jpg', target_size=(224, 224))
+image = load_img('images/47654254552_6b9a395051_z.jpg', target_size=(224, 224))
 # convert the image pixels to a numpy array
 image = img_to_array(image)
 # reshape data for the model
